4efcf7a528fb7a734654b312e6b9e0830857ff56
ruanzqy0081 <zultraffx1@gmail.com>
2026-03-11 11:07:34 +0000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -11,6 +11,8 @@ import Packages from "./pages/Packages";
 import Keys from "./pages/Keys";
 import Profile from "./pages/Profile";
 import Devices from "./pages/Devices";
+
+
 
 
 function Router() {
@@ -30,27 +32,30 @@ function Router() {
 }
 
 
+
+
 // NOTE: About Theme
 // - First choose a default theme according to your design style (dark or light bg), than change color palette in index.css
 //   to keep consistent foreground/background color across components
 // - If you want to make theme switchable, pass `switchable` ThemeProvider and use `useTheme` hook
 
 
+
+
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider
-        defaultTheme="light"
-        // switchable
-      >
+      
         <TooltipProvider>
           <Toaster />
           <Router />
         </TooltipProvider>
-      </ThemeProvider>
+      
     </ErrorBoundary>
   );
 }
+
+
 
 
 export default App;
